756ed0fd4745d0e5e67a4b0b146d205a2c95dfbf
devogs <billy.bob@devogs.xyz>
2025-05-16 15:16:32 +0200

    stage 3 completed

--- app/litreview/admin.py
+++ app/litreview/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Ticket, Review, UserFollows
+
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
