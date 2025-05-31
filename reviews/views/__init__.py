c319202a851f284c0e3ae1c0bcb884a66166bb5b
devogs <billy.bob@devogs.xyz>
2025-05-31 17:32:13 +0200

    Resolve feed import errors & enhance logic

--- reviews/views/__init__.py
+++ reviews/views/__init__.py
@@ -4,3 +4,4 @@ from .main_views import flux, user_posts
 from .follow_views import following, unfollow
 from .ticket_views import ticket_create, ticket_update, ticket_delete
 from .review_views import review_create, ticket_and_review_create, review_update, review_delete
+from .utils_views import get_users_viewable_tickets, get_users_viewable_reviews
